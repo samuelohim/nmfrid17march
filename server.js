ca52d10ed62574a9f40b38d9c750bd4c2838cd49
Samuel Y <samuel.y@weimeit.com>
2017-03-17 10:36:34 +0800

    ejs layout web view implemented

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const eo = require('express');
 app = eo();
 port = process.env.PORT || 8080;
 
+ejl = require('express-ejs-layouts');
+
+app.use(ejl);
+app.set('view engine', 'ejs');
+eo.static(__dirname+'/public');
+
+
+
+
+
+
+
 app.use(require('./app/router.js'));
 
 app.listen(port, ()=>{
